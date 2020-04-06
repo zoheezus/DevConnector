a28e9db244785b1be72202edfffb7676b8288528
zoheezus <zohaib0126@gmail.com>
2020-04-06 17:20:20 -0400

    starting on dashboard

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -7,7 +7,8 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
-  LOGOUT
+  LOGOUT,
+  CLEAR_PROFILE,
 } from "./types";
 import setAuthToken from "../utils/setAuthToken";
 
@@ -49,7 +50,7 @@ export const register = ({ name, email, password }) => async (dispatch) => {
       payload: res.data,
     });
 
-    dispatch(loadUser())
+    dispatch(loadUser());
   } catch (err) {
     const errors = err.response.data.errors;
 
@@ -80,7 +81,7 @@ export const login = (email, password) => async (dispatch) => {
       payload: res.data,
     });
 
-    dispatch(loadUser())
+    dispatch(loadUser());
   } catch (err) {
     const errors = err.response.data.errors;
 
@@ -94,6 +95,7 @@ export const login = (email, password) => async (dispatch) => {
 };
 
 // Loutout / Clear Profile
-export const logout = () => dispatch => {
-  dispatch({ type: LOGOUT })
-}
+export const logout = () => (dispatch) => {
+  dispatch({ type: CLEAR_PROFILE });
+  dispatch({ type: LOGOUT });
+};
