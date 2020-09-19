638db883436239eb925e80f7fc791136587e157c
zoheezus <zohaib0126@gmail.com>
2020-09-18 21:57:49 -0400

    fix match.params.id warning

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -13,7 +13,7 @@ const Profile = ({
 }) => {
   useEffect(() => {
     getProfileById(match.params.id);
-  }, [getProfileById]);
+  }, [getProfileById, match.params.id]);
 
   return (
     // infinite loading error :(
